318a7be6fee4e735815c491a57bbc84658213ba2
jsm92 <36892755+jsm92@users.noreply.github.com>
2018-05-15 14:52:57 +0200

    Dodano przeciwnika

--- js/przeciwnik.js
+++ js/przeciwnik.js
@@ -1,7 +1,7 @@
 	var defaultpx = 24; //przeciwnik
 	var defaultpy = 24;
-	var rozmpx = 40;
-	var rozmpy = 40;
+	var rozmpx = 15;
+	var rozmpy = 15;
 	
 	
 	function przeciwnik() {
@@ -11,6 +11,16 @@
 		ctx.fillStyle="blue";
 		ctx.fillRect(defaultpx,defaultpy,rozmpx,rozmpy);
 		ctx.closePath;
-	
+
+
+		ctx.beginPath();
+		ctx.fillStyle="blue";
+		ctx.fillRect(defaultpx + 30,defaultpy,rozmpx,rozmpy);
+		ctx.closePath;
+
+		ctx.beginPath();
+		ctx.fillStyle="blue";
+		ctx.fillRect(defaultpx + 60,defaultpy,rozmpx,rozmpy);
+		ctx.closePath;
 	
 }
