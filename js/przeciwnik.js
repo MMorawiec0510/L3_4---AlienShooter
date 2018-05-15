18eb620241675345960a65e1a03900094f13d6b9
Mateusz <mefiu0510@gmail.com>
2018-05-15 14:43:13 +0200

    Alien

---
+++ js/przeciwnik.js
@@ -0,0 +1,16 @@
+	var defaultpx = 24; //przeciwnik
+	var defaultpy = 24;
+	var rozmpx = 40;
+	var rozmpy = 40;
+	
+	
+	function przeciwnik() {
+	
+	
+		ctx.beginPath();
+		ctx.fillStyle="blue";
+		ctx.fillRect(defaultpx,defaultpy,rozmpx,rozmpy);
+		ctx.closePath;
+	
+	
+}
